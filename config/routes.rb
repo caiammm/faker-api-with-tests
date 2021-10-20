3d43189558de15656e304df625d733287bcc81ad
renato caiam <caiam.rct@gmail.com>
2021-10-19 22:33:17 -0300

    feat: adds structure to separate routes files for wanted namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  controller :cars do
-    get '/cars/:model_number/car_can_be_built-' => :can_be_built, as: :car_can_be_built
-  end
+  extend CarsRoutes
 end
