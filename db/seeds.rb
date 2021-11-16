969d048dbb92084bd4f45f8c66fe4ae946373b7d
Caiam <caiam.rct@gmail.com>
2021-11-16 13:57:12 -0300

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,30 +15,30 @@ PlantingCultureInformation.create_or_find_by({
 
 
 cars = [
-  Car.create({
+  Car.create_or_find_by({
     name: "Chevetão",
     model_number: "chev-8855",
     year: 1990,
   }),
-  Car.create({
+  Car.create_or_find_by({
     name: "Fuscão",
     model_number: "fsc-9115",
     year: 1993,
   }),
 ]
 
-car_without_sales = Car.create({
+car_without_sales = Car.create_or_find_by({
     name: "Corola",
     model_number: "crl-1548",
     year: 2015,
   })
 
 countries = [
-  Country.create({
+  Country.create_or_find_by({
     name: "Brazil",
     population: 200_000_000
   }),
-  Country.create({
+  Country.create_or_find_by({
     name: "Germany",
     population: 30_000_000
   }),
@@ -46,7 +46,7 @@ countries = [
 
 cars.each do |car|
   countries.each do |country|
-    CarsSale.create({
+    CarsSale.create_or_find_by({
       car_id: car.id,
       total_sales: 1000,
       country_id: country.id,
@@ -57,14 +57,17 @@ end
 
 cars_with_sales_in_brazil = Car.joins(:countries).where(countries: {name: 'Brazil'}).select(:id)
 
+<<-SQL
 SELECT "cars"."id" FROM "cars"
 INNER JOIN "cars_sales" ON "cars_sales"."car_id" = "cars"."id"
 INNER JOIN "countries" ON "countries"."id" = "cars_sales"."country_id"
 WHERE "countries"."name" = 'Brazil'
+SQL
 
-
+cars_with_sales_in_brazil = Car.joins(:countries).where(countries: {name: 'Brazil'}).select(:id)
 Car.where.not(id: cars_with_sales_in_brazil)
 
+<<-SQL
 SELECT "cars".* FROM "cars"
 WHERE "cars"."id" NOT IN (
   SELECT "cars"."id" FROM "cars"
@@ -72,6 +75,7 @@ WHERE "cars"."id" NOT IN (
   INNER JOIN "countries" ON "countries"."id" = "cars_sales"."country_id"
   WHERE "countries"."name" = 'Brazil'
 )
+SQL
 
 # construindo queries na mão uma vez!
 Car.joins("INNER JOIN cars_sales cs ON cs.car_id = cars.id").where(cs: {id: '74005720-b1ce-4ade-8db2-2a18d465cccf'})
